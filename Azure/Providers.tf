b0c2c92588e0adcc0a776c8fbda9422a2ed94aef
Jeff Christman <jeff.christman@hotmail.com>
2023-02-17 07:50:45 -0600

    Update Providers.tf

--- Azure/Providers.tf
+++ Azure/Providers.tf
@@ -15,10 +15,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "be9a1fbd-6055-450e-9f93-e817e7c4570a"
-  tenant_id = "795e28b4-8802-4721-9a2e-c81f108d8536"
-  client_id =  "885d90fe-1dd7-4171-aa56-892e5d0e9e7f"
-  client_secret = "7Bu8Q~Bu_aJM6CAFL2GuRrkfdEnL2JmnfEDSDcRa"
+  subscription_id = ""
+  tenant_id = ""
+  client_id =  ""
+  client_secret = ""
 
 }
 
