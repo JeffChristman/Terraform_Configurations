36a3bb3069d58d1f93d3132b83e2ee4c541e9e42
Jeff Christman <jeff.christman@hotmail.com>
2023-04-27 10:50:45 -0500

    Add files via upload

---
+++ azure/main/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "prefix" {
+  type        = string
+  default     = "pslab-vm-srv"
+  description = "Prefix of the resource name"
+}
